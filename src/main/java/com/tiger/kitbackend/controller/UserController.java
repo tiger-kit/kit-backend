9b6a26c1c55bf4d49625527739ece3ed02166421
JZWen <ziwen_jiang@qq.com>
2023-09-21 22:48:57 +0800

    init project arch

---
+++ src/main/java/com/tiger/kitbackend/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.tiger.kitbackend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: JZWen
+ * @date:2023/9/21 10:47 下午
+ */
+@RestController
+public class UserController {
+
+
+}
---
+++ src/main/java/com/tiger/kitbackend/domain/Demo.java
@@ -0,0 +1,10 @@
+package com.tiger.kitbackend.domain;
+
+/**
+ * @author: JZWen
+ * @date:2023/9/21 10:47 下午
+ *
+ * 领域层设计
+ */
+public class Demo {
+}
