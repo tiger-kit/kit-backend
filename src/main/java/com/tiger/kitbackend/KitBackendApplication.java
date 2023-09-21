8866dabeece3ac7b135cc5e04213fc8f9a8d2e68
JZWen <ziwen_jiang@qq.com>
2023-09-21 22:42:43 +0800

    init project

---
+++ src/main/java/com/tiger/kitbackend/KitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tiger.kitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tiger/kitbackend/KitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiger.kitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
